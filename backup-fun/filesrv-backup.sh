0ad4a6a43a04814fead3ff85a8e50208a5d7de56
gravcat <gravcat@live.com>
2015-12-08 08:53:01 -0600

    Tar now pipes into split, because the service that is storing my files has a theoretical file size limit.

--- backup-fun/filesrv-backup.sh
+++ backup-fun/filesrv-backup.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 # A simple script to manage backups on a local file server. May become simpler or more complex as necessary.
+#	To "extract" the archive, "cat filesrv-backup-$TIME.tar.gz.* | tar xzvf" should do the trick in rejoining the files.
 # gravcat, 2015-12-07
 
 version=0.1a
 time=`date +%Y-%m-%d-%H.%M`
 inputdir=/mnt/backups/filesrv_1
 outputdir=/mnt/backups/upload-ready
-outputname=filesrv-backup-$TIME.tar.gz
+outputname=filesrv-backup-$TIME.tar.gz.
 api1=https://hooks.slack.com/
 api2=services/T0CUBMMGC/B0G3HBAR1/4WfH9labHb4BpnAxNhCOD4DX
-api3=$api1$api2
+api3=$API1$API2
 
 
-tar -cpzf $outputdir/$outputname $inputdir
-echo "Completed backup at $time."
+tar cpzf -$INPUTDIR/ | split --bytes=9.5GB - $OUTPUTDIR/`date +%Y-%m-%d`/$OUTPUTNAME 
+echo "Completed backup at $TIME." 
 
-curl -X POST --data-urlencode 'payload="text": "Sir, the filesrv backup has completed at $time.", "channel": "#monitoring", "username": "Codsworth", "iron_emoji": ":codsworth:"}' $api3
+curl -X POST --data-urlencode 'payload="text": "Sir, the filesrv backup has completed at $time.", "channel": "#monitoring", "username": "Codsworth", "iron_emoji": ":codsworth:"}' $API3
